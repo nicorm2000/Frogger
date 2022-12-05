594c97ddf08b56201bad3af8633b1f4d93f7d405
nicorm <nicorm2000@gmail.com>
2022-12-04 22:41:18 -0300

    Created the checkplayerinput function, it has the ability to exit the game with a key, and also lets the player move

--- Src/Window/PlayGame.cpp
+++ Src/Window/PlayGame.cpp
@@ -7,6 +7,8 @@ static void Initialize();
 
 static void Close();
 
+void CheckPlayerInput(Frog& frog, bool& playingGame);
+
 void GameLoop()
 {
 	Initialize();
@@ -24,6 +26,10 @@ void GameLoop()
 		BeginDrawing();
 
 		ClearBackground(BLACK);
+
+		CheckPlayerInput(frog, playingGame);
+
+		//Draw
 
 		DrawRectangle(510, 0, 2, 1024, WHITE);
 
@@ -46,4 +52,32 @@ static void Initialize()
 static void Close()
 {
 	CloseWindow();
+}
+
+void CheckPlayerInput(Frog& frog, bool& playingGame)
+{
+	if (IsKeyPressed(KEY_ENTER))
+	{
+		playingGame = false;
+	}
+
+	if (IsKeyPressed(KEY_UP))
+	{
+		frog.frogPosition.y -= 64;
+	}
+
+	if (IsKeyPressed(KEY_DOWN))
+	{
+		frog.frogPosition.y += 64;
+	}
+
+	if (IsKeyPressed(KEY_RIGHT))
+	{
+		frog.frogPosition.x += 64;
+	}
+
+	if (IsKeyPressed(KEY_LEFT))
+	{
+		frog.frogPosition.x -= 64;
+	}
 }
