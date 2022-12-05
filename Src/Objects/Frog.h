1cf77321ad77bc808792de3dfee55bb8e8c6fb18
nicorm <nicorm2000@gmail.com>
2022-12-04 22:05:39 -0300

    Created the frig struct and its two main functions

--- Src/Objects/Frog.h
+++ Src/Objects/Frog.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "raylib.h"
+
+struct Frog
+{
+	Vector2 frogPsition{};
+	Vector2 frogSize;
+	bool isAlive;
+	Color frogColor;
+	Texture frogTexture;
+};
+
+Frog CreateFrog(Frog& frog);
+
+void DrawFrog(Frog frog);
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,4 +1,4 @@
-
+#include "Window/PlayGame.h"
 
 int main()
 {
