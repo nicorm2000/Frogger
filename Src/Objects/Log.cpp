14943c6e069a4f4968ff352c3f2bd4f200e832f4
nicorm <nicorm2000@gmail.com>
2022-12-06 16:28:05 -0300

    Created log struct and createlog and drawlog function as well

--- Src/Objects/Log.cpp
+++ Src/Objects/Log.cpp
@@ -2,5 +2,44 @@
 
 namespace game
 {
+	Log CreateSmallLog(Log& log)
+	{
+		log.logPosition.x = -150;
+		log.logPosition.y = 256;
+		log.logSize.x = 180;
+		log.logSize.y = 60;
+		log.logSpeed = 250;
+		log.logColor = BROWN;
 
+		return log;
+	}
+
+	Log CreateMediumLog(Log& log)
+	{
+		log.logPosition.x = 1124;
+		log.logPosition.y = 320;
+		log.logSize.x = 240;
+		log.logSize.y = 60;
+		log.logSpeed = 250;
+		log.logColor = BROWN;
+
+		return log;
+	}
+
+	Log CreateBigLog(Log& log)
+	{
+		log.logPosition.x = -150;
+		log.logPosition.y = 384;
+		log.logSize.x = 300;
+		log.logSize.y = 60;
+		log.logSpeed = 250;
+		log.logColor = BROWN;
+
+		return log;
+	}
+
+	void DrawLog(Log log)
+	{
+		DrawRectangle(static_cast<int>(log.logPosition.x), static_cast<int>(log.logPosition.y), static_cast<int>(log.logSize.x), static_cast<int>(log.logSize.y), log.logColor);
+	}
 }
--- Src/Objects/Log.h
+++ Src/Objects/Log.h
@@ -1,6 +1,24 @@
 #pragma once
 
+#include "raylib.h"
+
 namespace game
 {
+	struct Log
+	{
+		Vector2 logPosition{};
+		Vector2 logSize;
+		float logSpeed;
+		bool isFrogOnLog;
+		Color logColor;
+		Texture logTexture;
+	};
 
+	Log CreateSmallLog(Log& log);
+	
+	Log CreateMediumLog(Log& log);
+	
+	Log CreateBigLog(Log& log);
+
+	void DrawLog(Log log);
 }
