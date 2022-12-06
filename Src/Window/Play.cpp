c6912d6f2329f786b8e2c013f0857646b09256bf
nicorm <nicorm2000@gmail.com>
2022-12-05 22:41:08 -0300

    Added Credits and link to itch user page

--- Src/Window/Play.cpp
+++ Src/Window/Play.cpp
@@ -284,6 +284,14 @@ namespace game
 
 					mousePosition = GetMousePosition();
 					
+					if (CheckCollisionPointRec(mousePosition, { static_cast<float>(GetScreenWidth() / 2) - 87.5f, static_cast<float>(GetScreenHeight() / 2) - 20, 190, 85 }))
+					{
+						if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+						{
+							OpenURL("https://nicorm.itch.io/");
+						}
+					}
+
 					if (CheckCollisionPointRec(mousePosition, { 10, 10, 45, 45 }))
 					{
 						if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
@@ -296,6 +304,13 @@ namespace game
 
 					DrawRectangle(10, 10, 45, 45, RED);
 					DrawText("x", 20, 5, 50, WHITE);
+
+					DrawRectangle(static_cast<int>(GetScreenWidth() / 2) - 0 - 87, static_cast<int>(GetScreenHeight() / 2) - 20, 190, 85, BLACK);
+					DrawRectangle(static_cast<int>(GetScreenWidth() / 2) - 0 - 82, static_cast<int>(GetScreenHeight() / 2) - 15, 180, 75, RED);
+					DrawText("Itch.io", static_cast<int>(GetScreenWidth() / 2) - MeasureText("Itch.io", 25), static_cast<int>(GetScreenHeight() / 2), 50, BLACK);
+					DrawRectangle(250, GetScreenHeight() / 2 - 115, 550, 85, BLACK);
+					DrawRectangle(255, GetScreenHeight() / 2 - 110, 540, 75, WHITE);
+					DrawText("Nicolas Ramos Marin", static_cast<int>(GetScreenWidth() / 2) - MeasureText("Nicolas Ramos Marin", 25), static_cast<int>(GetScreenHeight() / 2) - 100, 50, BLACK);
 
 					break;
 
